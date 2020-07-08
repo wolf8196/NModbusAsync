6b202ec98db5afced71657da2817e65fb3b19ebf
Roman Vovk <wolf8196@gmail.com>
2020-07-08 11:12:36 +0300

    add new factory method to interface

--- NModbusAsync/IModbusFactory.cs
+++ NModbusAsync/IModbusFactory.cs
@@ -5,5 +5,7 @@ namespace NModbusAsync
     public interface IModbusFactory
     {
         IModbusMaster CreateMaster<T>(T tcpClient) where T : TcpClient;
+
+        IModbusMaster CreateMaster<TResource>(TResource tcpClient, IModbusLogger logger) where TResource : TcpClient;
     }
 }
