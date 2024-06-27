1898d5e71531714b3d8a7b4da7abe019bfa33f89
Roman Vovk <wolf8196@gmail.com>
2024-06-27 14:22:03 +0300

    add timeout tests

---
+++ NModbusAsync.Test/Integration/RtuOverTcp/RtuOverTcpTimeoutTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Xunit.Abstractions;
+
+namespace NModbusAsync.Test.Integration.RtuOverTcp
+{
+    [ExcludeFromCodeCoverage]
+    public class RtuOverTcpTimeoutTest : TimeoutTest
+    {
+        public RtuOverTcpTimeoutTest(ITestOutputHelper output)
+        : base(RtuOverTcpMaster, output)
+        {
+        }
+    }
+}
